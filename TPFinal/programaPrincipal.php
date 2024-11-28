a0be677c659d91e6a7983cc90c64f1f6a6491e1c
IsisJulian <isis.julian@est.fi.uncoma.edu.ar>
2024-11-27 22:17:54 -0300

    moduloAuto

--- TPFinal/programaPrincipal.php
+++ TPFinal/programaPrincipal.php
@@ -7,26 +7,27 @@ $primaveraArray [10][3];
 $inviernoArray [5][3];
 $asociativoArray [1][3];
 
-//es un menu que da a elegir entre 10 opciones
+//          \n
 do{
-
-        echo "Realizar una carga automática de la matriz de temperaturas
-                a) Realizar una carga manual de la matriz de temperaturas.
-                b) Mostrar el contenido de la matriz por filas y columnas.
-                c) Mostrar, dado un año y un mes, el valor de temperatura correspondiente.
-                d) Mostrar para un determinado año, las temperaturas de todos los meses
-                e) Mostrar para un mes determinado, las temperaturas de todos los años y el promedio.
-                f) Hallar las temperaturas máximas y mínimas, indicando año y mes a los que corresponden. Si el máximo o mínimo se repite, mostrar el primero encontrado.
+        echo "Bienvenido al menu ingrese la opcion que desea de la letra a hasta i:\n ";
+        echo   "a) Realizar una carga automática de la matriz de temperaturas
+                b) Realizar una carga manual de la matriz de temperaturas.
+                c) Mostrar el contenido de la matriz por filas y columnas.
+                d) Mostrar, dado un año y un mes, el valor de temperatura correspondiente.
+                e) Mostrar para un determinado año, las temperaturas de todos los meses
+                f) Mostrar para un mes determinado, las temperaturas de todos los años y el promedio.
+                g) Hallar las temperaturas máximas y mínimas, indicando año y mes a los que corresponden. Si el máximo o mínimo se repite, mostrar el primero encontrado.
                 h) Crear y mostrar un arreglo bidimensional con los datos de primavera (oct-nov-dic) de todos los años.
                 i) Crear y mostrar un arreglo bidimensional con los datos de los últimos 5 años de invierno (jul-ago-sep).
                 j) Crear un arreglo asociativo que contenga en la primera posición con clave “completa” la matriz completa de temperaturas, en la segunda posición con clave “primavera” la matriz creada en el inciso h., y en la tercera posición con clave “invierno” la matriz creada en el inciso i.”
+                Ingrese su opcion elegida: \n
                 ";
         $opcion=trim(fgets(STDIN));
 
     
         switch ($opcion) {
             case "a":
-            
+                autoArr($autArray);
                 break;
             case "b":
             
@@ -56,9 +57,9 @@ do{
             
                 break;    
             default:
-                echo "Eligio una letra que no corresponde";
+                echo "Eligio una letra que no corresponde \n ";
         }
-        echo "Desea elegir otra opcion? (si/no)";
+        echo "Desea elegir otra opcion? (si/no): ";
         $rpta=trim(fgets(STDIN));
 }while($rpta=="si");
 
