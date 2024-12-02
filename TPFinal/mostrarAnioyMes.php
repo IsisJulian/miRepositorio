ef854a74132f127290b067a28afd41966eee2227
mauricioiba1 <mauricio.ibanez@est.fi.uncoma.edu.ar>
2024-12-01 21:20:32 -0300

    nuevoPrin

--- TPFinal/mostrarAnioyMes.php
+++ TPFinal/mostrarAnioyMes.php
@@ -1,9 +1,6 @@
 <?php
 /**
  * este modulo cargara con valores la matriz inicializada en el programa principal
-*@param float $autArray
-*@param int $aElegido
-*@param int $mElegido
 *@return array
 */
 function mostrarArrAyM($auto,$aElegido,$mElegido){
